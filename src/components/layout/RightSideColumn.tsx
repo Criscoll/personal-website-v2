d6593164329184955e7dcfd7557af3a4027cb71b
Cristian <cristian.collante@protonmail.com>
2021-08-31 17:39:58 +1000

    Added right column content -> resume text, icons and typewriter effect on icon hover

--- src/components/layout/RightSideColumn.tsx
+++ src/components/layout/RightSideColumn.tsx
@@ -1,18 +1,70 @@
+import { useState, useEffect } from 'react';
 import emailIcon from '../../assets/images/email-icon-white.png';
 import linkedinIcon from '../../assets/images/linkedin-icon-white.png';
 import githubIcon from '../../assets/images/github-icon-white.png';
+import Typewriter from 'typewriter-effect';
 
 export default function RightSideColumn() {
+    const [hoveredContact, setHoveredContact] = useState('');
+    const [renderTypewriter, setRenderTyperwriter] = useState(false);
+
+    useEffect(() => {
+        setRenderTyperwriter(false);
+        setTimeout(() => {
+            setRenderTyperwriter(true);
+        }, 0);
+    }, [hoveredContact]);
+
     return (
         <>
             <div className="side-column-right-container">
-                <div className="resume">
-                    <p>Resume</p>
-                </div>
-                <div className="contact-icons">
-                    <img src={emailIcon} alt="email" width="35px" />
-                    <img src={linkedinIcon} alt="linkedin" width="35px" />
-                    <img src={githubIcon} alt="github" width="35px" />
+                <h1 className="resume">Resume</h1>
+
+                <div className="contact-details">
+                    {renderTypewriter ? (
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter.typeString(hoveredContact).start();
+                            }}
+                            options={{
+                                delay: 60,
+                                wrapperClassName: 'typewriter-wrapper-column',
+                                cursorClassName: 'typewriter-cursor-column',
+                            }}
+                        />
+                    ) : null}
+                    <div className="contact-icons">
+                        <img
+                            src={emailIcon}
+                            alt="email"
+                            onMouseEnter={() => {
+                                setHoveredContact('Email');
+                            }}
+                            onMouseLeave={() => {
+                                setHoveredContact('');
+                            }}
+                        />
+                        <img
+                            src={linkedinIcon}
+                            alt="linkedin"
+                            onMouseEnter={() => {
+                                setHoveredContact('LinkedIn');
+                            }}
+                            onMouseLeave={() => {
+                                setHoveredContact('');
+                            }}
+                        />
+                        <img
+                            src={githubIcon}
+                            alt="github"
+                            onMouseEnter={() => {
+                                setHoveredContact('Github');
+                            }}
+                            onMouseLeave={() => {
+                                setHoveredContact('');
+                            }}
+                        />
+                    </div>
                 </div>
             </div>
         </>

8e2aa00737070e7faa4b3d915edbad8ef249b116
Cristian <cristian.collante@protonmail.com>
2021-09-05 20:24:29 +1000

    added some responsive css properties for smaller screen sizes, still a work in progress

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,21 @@
+import { aboutme } from '../constants/text';
+import parse from 'html-react-parser';
+import ProfilePhoto from '../assets/images/profile-photo.jpg';
+
 export default function AboutPage() {
     return (
         <>
-            <section className="about-page-container"></section>
+            <section className="about-page-container">
+                <div className="inner-container">
+                    <h1>{' > 01. About Me'}</h1>
+                    <div className="text-content">
+                        <p>{parse(aboutme)}</p>
+                    </div>
+                    <div className="image-content">
+                        <img src={ProfilePhoto} alt="profile photo" />
+                    </div>
+                </div>
+            </section>
         </>
     );
 }

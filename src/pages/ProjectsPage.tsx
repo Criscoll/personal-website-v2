084f91e150bfedb19766818a1684bb049ac9e50c
Cristian <cristian.collante@protonmail.com>
2021-10-15 12:37:25 +1100

    forgot to save this file on last commit :)

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -20,7 +20,6 @@ export default function ProjectsPage() {
                 <ProjectCard {...projectCardProps} />
                 <ProjectCard {...projectCardProps} />
                 <ProjectCard {...projectCardProps} />
-
             </section>
         </>
     );

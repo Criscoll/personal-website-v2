e8cea9b5c088b0bb943739065b505cef83b22e1d
Cristian <cristian.collante@protonmail.com>
2022-02-09 17:08:13 +1100

    Finished projects section

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,25 +1,16 @@
 import ProjectCard from "../components/card/ProjectCard";
+import { pathfinderProject, sortingAlgoVisualiserProject, photoproProject } from "../constants/text";
 
 export default function ProjectsPage() {
 
-    const projectCardProps = {
-        imgBtnProps: {
-            imgUrl: "https://drive.google.com/uc?export=view&id=1g2IkXiVfCdZXwlWmwumS-Dy_cLgQy3-w",
-            altText: "Visual Pathfinder Cover",
-            projectTitle: "Visual Pathfinder",
-            href: "https://github.com/Criscoll/personal-website-v2"
-        },
-        projectDescription: "Lorem ipsum bich",
-        projectTechStack: "Hi"
-    }
 
     return (
         <>
             <section className="projects-page-container">
-                <h1>{'> Projects'}</h1>
-                <ProjectCard {...projectCardProps} />
-                <ProjectCard {...projectCardProps} />
-                <ProjectCard {...projectCardProps} />
+                <h1>{'> 02. Projects'}</h1>
+                <ProjectCard {...pathfinderProject} />
+                <ProjectCard {...sortingAlgoVisualiserProject} />
+                <ProjectCard {...photoproProject} />
             </section>
         </>
     );
